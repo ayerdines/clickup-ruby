fb62d9b4aa1f86665d78ecc9ae9c399e696df196
Dinesh Budhayer <budhayer96d@gmail.com>
2021-01-15 14:30:57 +0545

    added namespace to generator

--- lib/generators/click_up/install_generator.rb
+++ lib/generators/click_up/install_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators'
 
 module ClickUp
   module Generators
-    class ClickUpGenerator < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
       desc "This generator creates click_up.rb file at config/initializers"
       def create_initializer_file
         create_file "config/initializers/click_up.rb", "# Add ClickUp.api_token here\nClickUp.api_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'"
